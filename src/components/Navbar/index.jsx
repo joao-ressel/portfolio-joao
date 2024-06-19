8eb2e52bd042bef476ddd212f359cbb2e0c88179
joao-ressel <joao.resselti@gmail.com>
2024-06-19 08:13:54 -0500

    refact: styling the header

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,20 @@
 import { Link } from 'react-router-dom';
-
+import {LinkedinLogo, InstagramLogo, GithubLogo} from "@phosphor-icons/react"
 export const Navbar = () => {
   return (
     <nav className="nav">
-      <div className="logo"><Link to="/">Logo</Link></div>
+     <Link  to="/">
+        <div className="logo"></div>
+     </Link>
       <ul className="navlinks">
         <li><Link to="/about">ABOUT</Link></li>
         <li><Link to="/works">PROJECTS</Link></li>
         <li><Link to="/contact">CONTACT</Link></li>
       </ul>
       <div className="icons">
-        <a href="#">GT</a>
-        <a href="#">IN</a>
-        <a href="#">IG</a>
+        <a href="#"><LinkedinLogo size={24} weight="fill"/></a>
+        <a href="#"><InstagramLogo size={24} weight="fill"/></a>
+        <a href="#"><GithubLogo size={24} weight="fill"/></a>
       </div>
     </nav>
   );
