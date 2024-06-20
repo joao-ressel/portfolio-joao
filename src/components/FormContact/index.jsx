b127e18ccdbbab13d1d4f32e8360ddf10aecb576
joao-ressel <joao.resselti@gmail.com>
2024-06-20 14:05:43 -0500

    refact: adding styling changes to the contact component and installing emailJS library

--- src/components/FormContact/index.jsx
+++ src/components/FormContact/index.jsx
@@ -1,23 +1,70 @@
-import {Button} from "../../components/Button"
+import { useState } from "react";
+import { Button } from "../../components/Button";
+import emailjs from "@emailjs/browser";
+
 export const FormContact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMenssage] = useState("");
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    if (name === "" || email === "" || message === "") {
+      alert("Preencha todos os campos");
+      return;
+    }
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email,
+    };
+    emailjs
+      .send(
+        "service_4me8nml",
+        "template_gufg791",
+        templateParams,
+        "mNEYj85spEiEvLzq8"
+      )
+      .then(
+        (response) => {
+          console.log("Enviado", response.status, response.text);
+          setName("");
+          setEmail("");
+          setMenssage("");
+        },
+        (err) => {
+          console.log("Erro:", err);
+        }
+      );
+  }
+
   return (
     <section className="contactForm">
       <h2>Drop me a line. I would like to hear from you.</h2>
-      <form>
+      <form onSubmit={sendEmail}>
         <h3>Get in Touch</h3>
-        <input type="text" name="name" className="input" placeholder="Name" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <input
           type="email"
           name="email"
-          className="input"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <textarea
           name="message"
-          className="textarea"
           placeholder="Message"
+          value={message}
+          onChange={(e) => setMenssage(e.target.value)}
         ></textarea>
 
         <Button variant="rose">Send</Button>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sass from 'vite-plugin-sass';
 
 export default defineConfig({
   plugins: [
     react(),
-    sass()
   ]
 });
