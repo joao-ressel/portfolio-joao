9bb024582f89e157246e299d0d95c8a5cb3376b5
joao-ressel <joao.resselti@gmail.com>
2024-06-21 12:28:36 -0500

    refact: styling general styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,23 @@ import { Footer } from "./components/Footer";
 import { Projects } from "./pages/Projects";
 import { EasyLab } from "./pages/Projects/EasyLab";
 import { Ideal } from "./pages/Projects/Ideal";
+import { Container } from "./components/Container";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
 
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/easylab" element={<EasyLab />} />
-        <Route path="/projects/ideal" element={<Ideal />} />
-      </Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/easylab" element={<EasyLab />} />
+          <Route path="/projects/ideal" element={<Ideal />} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
